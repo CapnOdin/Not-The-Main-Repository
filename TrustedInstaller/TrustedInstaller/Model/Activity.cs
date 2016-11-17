ae5e49bdca0a1ab3f534e4714da6c6e188770a23
Matt <Matt@DESKTOP-0UPRFN0>
2016-11-17 16:41:56 +0100

    Added 2 models, the super class ShapeModel and the first model Activity, nothing is working at the moment.

---
+++ TrustedInstaller/TrustedInstaller/Model/Activity.cs
@@ -0,0 +1,42 @@
+﻿namespace TrustedInstaller.Model {
+    using System;
+    using System.Windows;
+    using System.Windows.Media;
+
+    class Activity : ShapeModel {
+
+        public static readonly DependencyProperty RectWidthProperty = DependencyProperty.Register("Width", typeof(Double), typeof(Activity), new PropertyMetadata(0.0));
+        public static readonly DependencyProperty RectHeightProperty = DependencyProperty.Register("Length", typeof(Double), typeof(Activity), new PropertyMetadata(0.0));
+
+        public Activity(bool nameable, bool connectable, string name = "", string ID = "") : base(nameable, connectable, name, ID) {
+
+        }
+
+        protected override Geometry DefiningGeometry {
+            get {
+                Rect rect = new Rect(0, 0, this.RectWidth, this.RectHeight);
+                Geometry g = new RectangleGeometry(rect);
+                return g;
+            }
+        }
+
+        public Double RectHeight {
+            get {
+                return (Double)this.GetValue(RectHeightProperty);
+            }
+            set {
+                this.SetValue(RectHeightProperty, value);
+            }
+        }
+
+        public Double RectWidth {
+            get {
+                return (Double)this.GetValue(RectWidthProperty);
+            }
+            set {
+                this.SetValue(RectWidthProperty, value);
+            }
+        }
+
+    }
+}
---
+++ TrustedInstaller/TrustedInstaller/Model/ShapeModel.cs
@@ -0,0 +1,50 @@
+﻿namespace TrustedInstaller.Model {
+    using System.ComponentModel;
+    using System.Windows.Shapes;
+
+    public abstract class ShapeModel : Shape, INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name {
+            get; set;
+        }
+        private string ID {
+            get; set;
+        }
+
+        private bool nameable = false;
+        private bool connectable = false;
+
+        public ShapeModel(bool nameable, bool connectable, string name = "", string ID = "") {
+            if(nameable) {
+                this.name = name;
+            }
+            this.nameable = nameable; this.connectable = connectable; this.ID = ID;
+        }
+
+        public bool isConnectable {
+            get {
+                return connectable;
+            }
+            set {
+                connectable = value;
+            }
+        }
+
+        public bool isNameable {
+            get {
+                return nameable;
+            }
+            set {
+                nameable = value;
+            }
+        }
+
+        private void NotifyPropertyChanged(string propertyName) {
+            if(PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
