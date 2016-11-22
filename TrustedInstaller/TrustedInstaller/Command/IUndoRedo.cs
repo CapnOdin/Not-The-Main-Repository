0fb1d2b02004e315f8e098d08e51cbbb8c437707
Matt <Matt@DESKTOP-0UPRFN0>
2016-11-22 22:42:25 +0100

    Stuff for master captn' odinyo

---
+++ TrustedInstaller/TrustedInstaller/Command/IUndoRedo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrustedInstaller.Command {
+    interface IUndoRedo {
+
+        // The "Execute()" function of the example.
+        void DoRedo();
+
+        // The "UnExecute()" function of the example.
+        void Undo();
+    }
+}
